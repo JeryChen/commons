5e7cfeae65510a32439501909cc4de7b47e7a25f
杰瑞 <jierui@tongbanjie.com>
2019-11-12 16:01:55 +0800

    Initial commit

---
+++ src/test/java/com/ars/commons/ArsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
